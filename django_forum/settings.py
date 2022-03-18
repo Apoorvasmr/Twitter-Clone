208b3cf6f6fee6822631eabc883fdd57b842d35b
Musubi Management <musubimanagement@Musubis-MacBook-Air-2.local>
2022-03-18 08:55:48 +0530

    changed database

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -83,23 +83,23 @@ WSGI_APPLICATION = 'django_forum.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd9pja5ug6vnp6n',
-        'USER': 'oswrmpmmtrxcvm',
-        'HOST': 'ec2-52-44-209-165.compute-1.amazonaws.com',
-        'PORT': 5432,
-        'PASSWORD': '6f03ebed5bd3c7efc8b532b510803d0b6d54f69ad39e5a09364f8dcd0c72a772',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'd9pja5ug6vnp6n',
+#         'USER': 'oswrmpmmtrxcvm',
+#         'HOST': 'ec2-52-44-209-165.compute-1.amazonaws.com',
+#         'PORT': 5432,
+#         'PASSWORD': '6f03ebed5bd3c7efc8b532b510803d0b6d54f69ad39e5a09364f8dcd0c72a772',
+#     }
+# }
 
 
 # Password validation
@@ -126,7 +126,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asiaa/Kolkata'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
